8d12d7ed917da26f8d45f7da60987ab89ab9318d
Damien <zara.damien21@gmail.com>
2019-09-13 10:43:54 +0200

    Fix registration and ZIP file

--- upload/inc/plugins/oa_social_login/communication.php
+++ upload/inc/plugins/oa_social_login/communication.php
@@ -22,7 +22,7 @@
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  */
-define('OA_SOCIAL_LOGIN_USER_AGENT', 'SocialLogin/2.8.0 myBB/1.8 (+http://www.oneall.com/)');
+define('OA_SOCIAL_LOGIN_USER_AGENT', 'SocialLogin/2.8.1 myBB/1.8 (+http://www.oneall.com/)');
 
 /**
  * Sends an API request by using the given handler.
